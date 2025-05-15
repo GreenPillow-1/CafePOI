834098ec92b380fb2dc02dd3a8016ee63078bcb2
John <g15451452@gmail.com>
2025-05-15 12:00:29 +1000

    attempt for csv input

--- app/home.tsx
+++ app/home.tsx
@@ -1,4 +1,5 @@
-import React, { useRef, useState } from 'react';
+import Papa from 'papaparse';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   Alert,
   Dimensions,
@@ -24,108 +25,58 @@ interface Cafe {
 export default function Home() {
   const mapRef = useRef<MapView | null>(null);
 
+  const [cafes, setCafes] = useState<Cafe[]>([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedCafe, setSelectedCafe] = useState<Cafe | null>(null);
   const [userCafes, setUserCafes] = useState<Cafe[]>([]);
   const [selectedLocation, setSelectedLocation] = useState<{ latitude: number; longitude: number } | null>(null);
 
-  // User form
   const [newCafeName, setNewCafeName] = useState('');
-  const [newCafeAddress, setNewCafeAddress] = useState('');
-  const [selectedEmoji, setSelectedEmoji] = useState('🍩'); // default emoji
+  const [newCafeReview, setNewCafeReview] = useState('');
+  const [selectedEmoji, setSelectedEmoji] = useState('🍩');
 
   const emojiOptions = ['🍩', '🍔', '🍕', '🍣', '🍜', '🍺', '🍷', '🧁', '🧋'];
 
-const cafes: Cafe[] = [
-  { name: "Mack Daddy Soprano", address: "Unit 1, Basement, 313-315 Flinders Lane MELBOURNE 3000", latitude: -37.81764132, longitude: 144.96386031352995, emoji: "☕️" },
-  { name: "Grand Mercure Hotel", address: "321 Flinders Lane MELBOURNE 3000", latitude: -37.81773299, longitude: 144.9637651207598, emoji: "☕️" },
-  { name: "Glick's Cakes & Bagels", address: "Part Ground, 325 Flinders Lane MELBOURNE 3000", latitude: -37.81775969, longitude: 144.9635004630143, emoji: "☕️" },
-  { name: "Pronto On Flinders", address: "Part Ground, 335 Flinders Lane MELBOURNE 3000", latitude: -37.81784851, longitude: 144.96323530453128, emoji: "☕️" },
-  { name: "Bluestone", address: "349 Flinders Lane MELBOURNE 3000", latitude: -37.81787421, longitude: 144.9628671327705, emoji: "☕️" },
-  { name: "Degani Bakery Cafe", address: "Ground, 353 Flinders Lane MELBOURNE 3000", latitude: -37.81786117, longitude: 144.9627541135865, emoji: "☕️" },
-  { name: "Rendezvous Hotels", address: "328 Flinders Street MELBOURNE 3000",latitude: -37.81819655, longitude: 144.96357963298587, emoji: "☕️" },
-  { name: "Buffet Brasserie", address: "Shop 1, Ground, 300 Flinders Street MELBOURNE 3000", latitude: -37.81805897, longitude: 144.96400528370816, emoji: "☕️" },
-  { name: "Vita Juice Energy For Life", address: "298 Flinders Street MELBOURNE 3000", latitude: -37.81807376, longitude: 144.9643643, emoji: "☕️" },
-  { name: "Kimchi Tray", address: "294 Flinders Street MELBOURNE 3000", latitude: -37.81807376, longitude: 144.9643643, emoji: "☕️" },
-  { name: "Cafe 34", address: "Shop 2, 34 Queen Street MELBOURNE 3000", latitude: -37.81799107, longitude: 144.96244692064585, emoji: "☕️" },
-  { name: "Hudsons Coffee", address: "Ground, 359-365 Queen Street MELBOURNE 3000", latitude: -37.81799107, longitude: 144.96244692064585, emoji: "☕️" },
-  { name: "Maha Bar & Grill", address: "Basement, 21-27 Queen Street MELBOURNE 3000", latitude: -37.81819441, longitude: 144.96253974509804, emoji: "☕️" },
-  { name: "Migo's", address: "Shop 2, 289 Flinders Lane MELBOURNE 3000", latitude: -37.81718804, longitude: 144.96494250965242, emoji: "☕️" },
-  { name: "Box Coffee", address: "10-282 Flinders Street MELBOURNE 3000", latitude: -37.81766129, longitude: 144.9650952, emoji: "☕️" },
-  { name: "Roule Galette", address: "5 Flinders Lane MELBOURNE 3000", latitude: -37.81686107, longitude: 144.96631217224734, emoji: "☕️" },
-  { name: "The Journal Cafe", address: "Part Ground, 253-265 Flinders Lane MELBOURNE 3000", latitude: -37.81706442, longitude: 144.9659121030714, emoji: "☕️" },
-  { name: "Rice n' Rolls", address: "33 Flinders Lane MELBOURNE 3000",latitude: -37.81710426, longitude: 144.9654855543039, emoji: "☕️" },
-  { name: "Superfino Delicatessen", address: "275-275 Flinders Lane MELBOURNE 3000", latitude: -37.81710426, longitude: 144.9654855543039, emoji: "☕️" },
-  { name: "Indulgence Corner Bakery - Cafe", address: "37 Flinders Lane MELBOURNE 3000", latitude: -37.81710426, longitude: 144.9654855543039, emoji: "☕️" },
-  { name: "La Citta", address: "11 Flinders Street MELBOURNE 3000", latitude: -37.81760872, longitude: 144.96563502357697, emoji: "☕️" },
-  { name: "Red Nine", address: "15 Flinders Street MELBOURNE 3000", latitude: -37.81760872, longitude: 144.96563502357697, emoji: "☕️" },
-  { name: "Waffle On", address: "20 Flinders Street MELBOURNE 3000", latitude: -37.81752071, longitude: 144.9660065689182, emoji: "☕️" },
-  { name: "Flora Indian Restaurant", address: "238A Flinders Street MELBOURNE 3000", latitude: -37.81736333, longitude: 144.96637202927735, emoji: "☕️" },
-  { name: "Ratee Thai", address: "Shop 3, Flinders Street MELBOURNE 3000", latitude: -37.81724956, longitude: 144.96656101271213, emoji: "☕️" },
-  { name: "Jolly J's", address: "Shop 8, Flinders Street MELBOURNE 3000", latitude: -37.81724956, longitude: 144.96656101271213, emoji: "☕️" },
-  { name: "Kim Sing Chinese Restaurant", address: "Shop 10, Flinders Street MELBOURNE 3000", latitude: -37.81724956, longitude: 144.96656101271213, emoji: "☕️" },
-  { name: "Koury's Coffee Lounge", address: "Part Ground, 389 Market Street MELBOURNE 3000", latitude: -37.81850986, longitude: 144.9611428362183, emoji: "☕️" },
-  { name: "Subway", address: "Unit 3, Ground, 1-5 Elizabeth Street MELBOURNE 3000", latitude: -37.81805797, longitude: 144.96461741084877, emoji: "☕️" },
-  { name: "Pepperoni's", address: "7A Elizabeth Street MELBOURNE 3000", latitude: -37.8179333, longitude: 144.9645599261084, emoji: "☕️" },
-  { name: "McDonald's", address: "11-15 Elizabeth Street MELBOURNE 3000", latitude: -37.8178304, longitude: 144.9645087687041, emoji: "☕️" },
-  { name: "Mack Daddy Soprano", address: "Unit 1, Basement, 313-315 Flinders Lane MELBOURNE 3000", latitude: -37.81764132, longitude: 144.96386031352995, emoji: "☕️" },
-  { name: "Grand Mercure Hotel", address: "321 Flinders Lane MELBOURNE 3000", latitude: -37.81773299, longitude: 144.9637651207598, emoji: "☕️" },
-  { name: "Glick's Cakes & Bagels", address: "Part Ground, 325 Flinders Lane MELBOURNE 3000", latitude: -37.81775969, longitude: 144.9635004630143, emoji: "☕️" },
-  { name: "Pronto On Flinders", address: "Part Ground, 335 Flinders Lane MELBOURNE 3000", latitude: -37.81784851, longitude: 144.96323530453128, emoji: "☕️" },
-  { name: "Bluestone", address: "349 Flinders Lane MELBOURNE 3000", latitude: -37.81787421, longitude: 144.9628671327705, emoji: "☕️" },
-  { name: "Degani Bakery Cafe", address: "Ground, 353 Flinders Lane MELBOURNE 3000", latitude: -37.81786117, longitude: 144.9627541135865, emoji: "☕️" },
-  { name: "Rendezvous Hotels", address: "328 Flinders Street MELBOURNE 3000", latitude: -37.81819655, longitude: 144.96357963298587, emoji: "☕️" },
-    { name: "Hudsons Coffee", address: "84-86 Elizabeth Street MELBOURNE 3000", latitude: -37.81591429, longitude: 144.96419158047297, emoji: "☕️" },
-  { name: "Hudsons Coffee", address: "84-86 Elizabeth Street MELBOURNE 3000", latitude: -37.81591429, longitude: 144.96419158047297, emoji: "☕️" },
-  { name: "C & B", address: "41-43 Little Collins Street MELBOURNE 3000", latitude: -37.81529087, longitude: 144.96423478944925, emoji: "☕️" },
-  { name: "Brown Sugar Cafe", address: "25 Little Collins Street MELBOURNE 3000", latitude: -37.81529087, longitude: 144.96423478944925, emoji: "☕️" },
-  { name: "Brown Sugar Cafe", address: "25 Little Collins Street MELBOURNE 3000", latitude: -37.81529087, longitude: 144.96423478944925, emoji: "☕️" },
-  { name: "Cafe Meditteranean", address: "Shop 10, Ground, 93-107 Collins Street MELBOURNE 3000", latitude: -37.81529824, longitude: 144.9661124397802, emoji: "☕️" },
-  { name: "Cafe Meditteranean", address: "Shop 10, Ground, 93-107 Collins Street MELBOURNE 3000", latitude: -37.81529824, longitude: 144.9661124397802, emoji: "☕️" },
-  { name: "Thai Viet (Kiosk)", address: "115 Swanston Street MELBOURNE 3000", latitude: -37.81507151, longitude: 144.96579002216492, emoji: "☕️" },
-  { name: "Delicious Delights", address: "Shop 12, 115 Swanston Street MELBOURNE 3000", latitude: -37.81507151, longitude: 144.96579002216492, emoji: "☕️" },
-  { name: "Bistrot D'Orsay", address: "184 Collins Street MELBOURNE 3000", latitude: -37.81485337, longitude: 144.967286, emoji: "☕️" },
-  { name: "Bistrot D'Orsay", address: "184 Collins Street MELBOURNE 3000", latitude: -37.81485337, longitude: 144.967286, emoji: "☕️" },
-  { name: "Basso", address: "Shop 11, Basement, 195 Collins Street MELBOURNE 3000", latitude: -37.81450425, longitude: 144.9678570562652, emoji: "☕️" },
-  { name: "The Victoria Hotel", address: "215 Little Collins Street MELBOURNE 3000", latitude: -37.81446635, longitude: 144.9670901938735, emoji: "☕️" },
-  { name: "Comme", address: "7-19 Alfred Place MELBOURNE 3000", latitude: -37.81401415, longitude: 144.96991981992733, emoji: "☕️" },
-  { name: "Lotto off Collins", address: "Shop, 120 Collins Street MELBOURNE 3000", latitude: -37.81400168, longitude: 144.9693250884545, emoji: "☕️" },
-  { name: "Janil's Cafe", address: "Shop 1, 100 Collins Street MELBOURNE 3000", latitude: -37.81424109, longitude: 144.9700212911386, emoji: "☕️" },
-  { name: "Dr Martin's Tavern", address: "86A Collins Street MELBOURNE 3000", latitude: -37.81393809, longitude: 144.97059378480083, emoji: "☕️" },
-  { name: "Mediterranean Deli", address: "Shop 6, 80 Collins Street MELBOURNE 3000", latitude: -37.81364064, longitude: 144.97069571393286, emoji: "☕️" },
-  { name: "Stamford Plaza Hotel", address: "109-119 Little Collins Street MELBOURNE 3000", latitude: -37.81354692, longitude: 144.97003477811086, emoji: "☕️" },
-  { name: "Paris End Cafe", address: "Shop 2, Ground, 20 Collins Street MELBOURNE 3000", latitude: -37.81330625, longitude: 144.97290153805756, emoji: "☕️" },
-  { name: "Engel's Sandwich Bar & Delicatessen", address: "Shop 1, Ground, 20 Collins Street MELBOURNE 3000", latitude: -37.81330625, longitude: 144.97290153805756, emoji: "☕️" },
-  { name: "Nook Cafe", address: "19 Spring Street MELBOURNE 3000", latitude: -37.81278986, longitude: 144.97319412608084, emoji: "☕️" },
-  { name: "Spring Cafe & Larder", address: "9-11 Spring Street MELBOURNE 3000", latitude: -37.81260097, longitude: 144.97319174333387, emoji: "☕️" },
-  { name: "Excello Cafe", address: "Ground, 1-3 Spring Street MELBOURNE 3000", latitude: -37.81260097, longitude: 144.97319174333387, emoji: "☕️" },
-  { name: "Royal Melbourne Hotel", address: "621-633 Bourke Street MELBOURNE 3000", latitude: -37.8168684, longitude: 144.95523720563205, emoji: "☕️" },
-  { name: "The Bourke Armoury Cafe", address: "Part Ground, 655 Bourke Street MELBOURNE 3000", latitude: -37.81732136, longitude: 144.9545026234553, emoji: "☕️" },
-  { name: "St Arnou", address: "582-584 Little Collins Street MELBOURNE 3000", latitude: -37.81742034, longitude: 144.95536140545556, emoji: "☕️" },
-    { name: "Cafe Diy", address: "Shop 20, 121 William Street MELBOURNE 3000", latitude: -37.81636672, longitude: 144.95782326528942, emoji: "☕️" },
-  { name: "Cafenatics Espresso Bar", address: "Shop 18, 121 William Street MELBOURNE 3000", latitude: -37.81636672, longitude: 144.95782326528942, emoji: "☕️" },
-  { name: "Cafe Pazzo", address: "Shop 15, 121 William Street MELBOURNE 3000", latitude: -37.81636672, longitude: 144.95782326528942, emoji: "☕️" },
-  { name: "St James Take Away Food", address: "Shop 13, 121 William Street MELBOURNE 3000", latitude: -37.81636672, longitude: 144.95782326528942, emoji: "☕️" },
-  { name: "Lure's Gourmet Sandwich Bar", address: "Part Ground, 575 Bourke Street MELBOURNE 3000", latitude: -37.81651449, longitude: 144.95667920481293, emoji: "☕️" },
-  { name: "Lure's Gourmet Sandwich Bar", address: "Part Ground, 575 Bourke Street MELBOURNE 3000", latitude: -37.81651449, longitude: 144.95667920481293, emoji: "☕️" },
-  { name: "Nex Cafe", address: "Part Ground, 140-146 King Street MELBOURNE 3000", latitude: -37.81673697, longitude: 144.95628515442587, emoji: "☕️" },
-  { name: "Bambini Barrista", address: "Shop 7, 530 Little Collins Street MELBOURNE 3000", latitude: -37.81688112, longitude: 144.9571194391247, emoji: "☕️" },
-  { name: "Salto Cafe & Bistro", address: "Shop 9, 532 Little Collins Street MELBOURNE 3000", latitude: -37.81688112, longitude: 144.9571194391247, emoji: "☕️" },
-  { name: "Madisons Restaurant Bar Lounge", address: "Part Ground, 461 Bourke Street MELBOURNE 3000", latitude: -37.81549329, longitude: 144.96018234285577, emoji: "☕️" },
-  { name: "Cafe Felice", address: "Part Ground, 461 Bourke Street MELBOURNE 3000", latitude: -37.81549329, longitude: 144.96018234285577, emoji: "☕️" },
-  { name: "RACV City Club", address: "501-501 Bourke Street MELBOURNE 3000", latitude: -37.81588698, longitude: 144.9595939, emoji: "☕️" },
-  { name: "CQ Bar", address: "Part Mezzanine 0, 123-0 Queen Street MELBOURNE 3000", latitude: -37.81581364, longitude: 144.9606455105183, emoji: "☕️" },
-  { name: "Hudsons Coffee", address: "Part Ground, 455 Bourke Street MELBOURNE 3000", latitude: -37.8152734, longitude: 144.96051681010874, emoji: "☕️" },
-  { name: "Basso", address: "Shop 16, 385 Bourke Street MELBOURNE 3000", latitude: -37.81493078, longitude: 144.9629309900082, emoji: "☕️" },
-  { name: "Delicious Malaysian Cuisine & Vietnam Noodle", address: "Shop 10, 399-403 Bourke Street MELBOURNE 3000", latitude: -37.81513905, longitude: 144.96220519105626, emoji: "☕️" },
-  
+  // Load cafés from CSV on mount
+  useEffect(() => {
+    const csvUrl =
+      'https://raw.githubusercontent.com/GreenPillow-1/CafePOI/refs/heads/main/assets/cafes.csv?token=GHSAT0AAAAAADCOHXOQ2YF45CMY3P7Z7N5S2BFJAPA';
 
-
-];
+    fetch(csvUrl)
+      .then((response) => response.text())
+      .then((csvText) => {
+        Papa.parse(csvText, {
+          header: true,
+          skipEmptyLines: true,
+          complete: (results) => {
+            const parsed: Cafe[] = results.data.map((row: any) => {
+              if (
+                row.name &&
+                row.address &&
+                row.latitude &&
+                row.longitude
+              ) {
+                return {
+                  name: row.name,
+                  address: row.address,
+                  emoji: row.emoji || '☕️',
+                  latitude: parseFloat(row.latitude),
+                  longitude: parseFloat(row.longitude),
+                };
+              }
+              return null;
+            }).filter(Boolean) as Cafe[];
+            setCafes(parsed);
+          },
+          error: (error: any) => console.error('CSV parsing error', error),
+        });
+      })
+      .catch((err) => console.error('Failed to load CSV:', err));
+  }, []);
 
   const allCafes = [...cafes, ...userCafes];
 
-  const filteredCafes = allCafes.filter((cafe: Cafe) =>
+  const filteredCafes = allCafes.filter((cafe) =>
     cafe.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
@@ -153,23 +104,24 @@ const cafes: Cafe[] = [
       return;
     }
 
-    if (!newCafeName.trim() || !newCafeAddress.trim()) {
-      Alert.alert('Please fill in both the name and address');
+    if (!newCafeName.trim() || !newCafeReview.trim()) {
+      Alert.alert('Please fill in both the name and review');
       return;
     }
 
     const newCafe: Cafe = {
       name: newCafeName.trim(),
-      address: newCafeAddress.trim(),
+      address: newCafeReview.trim(),
       emoji: selectedEmoji,
       latitude: selectedLocation.latitude,
       longitude: selectedLocation.longitude,
     };
 
-    setUserCafes(prev => [...prev, newCafe]);
+    setUserCafes((prev) => [...prev, newCafe]);
 
+    // Reset form
     setNewCafeName('');
-    setNewCafeAddress('');
+    setNewCafeReview('');
     setSelectedEmoji('🍩');
     setSelectedLocation(null);
 
@@ -177,10 +129,7 @@ const cafes: Cafe[] = [
   };
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-      style={{ flex: 1 }}
-    >
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined} style={{ flex: 1 }}>
       <ScrollView contentContainerStyle={styles.container}>
         <Text style={styles.title}>Café & Restaurant Finder</Text>
 
@@ -218,11 +167,8 @@ const cafes: Cafe[] = [
             onPress={handleMapPress}
           >
             {filteredCafes.map((cafe, index) => (
-              <Marker
-                key={index}
-                coordinate={{ latitude: cafe.latitude, longitude: cafe.longitude }}
-              >
-                <Text style={{ fontSize: 28 }}>{cafe.emoji}</Text>
+              <Marker key={index} coordinate={{ latitude: cafe.latitude, longitude: cafe.longitude }}>
+                <Text style={{ fontSize: 28 }}>{cafe.emoji || '☕️'}</Text>
                 <Callout>
                   <View style={{ width: 200 }}>
                     <Text style={{ fontWeight: 'bold' }}>{cafe.name}</Text>
@@ -233,10 +179,7 @@ const cafes: Cafe[] = [
             ))}
 
             {selectedLocation && (
-              <Marker
-                coordinate={selectedLocation}
-                pinColor="green"
-              />
+              <Marker coordinate={selectedLocation} pinColor="green" />
             )}
           </MapView>
         </View>
@@ -250,14 +193,14 @@ const cafes: Cafe[] = [
             placeholder="Name"
             value={newCafeName}
             onChangeText={setNewCafeName}
-            placeholderTextColor={'black'}
+            placeholderTextColor="black"
           />
           <TextInput
             style={styles.formInput}
             placeholder="Review"
-            value={newCafeAddress}
-            onChangeText={setNewCafeAddress}
-            placeholderTextColor={'black'}
+            value={newCafeReview}
+            onChangeText={setNewCafeReview}
+            placeholderTextColor="black"
           />
 
           <Text style={{ marginVertical: 8 }}>Pick an icon:</Text>
